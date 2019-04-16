65f20158de91c892ffc473429a94716c6f60b125
mahendravarmak <49528392+mahendravarmak@users.noreply.github.com>
2019-04-16 22:15:22 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,47 +1,149 @@
 #include<stdio.h>
-struct proc
-{
-	int processid;
-	int bursttime;
-	int arrivaltime;
-	int ct=0;
-};
 
 int main()
+
 {
-	int n;
-	printf("enter how many process u want to enter\n");
-	scanf("%d",&n);
-	proc p[n];
-	for(int i=0;i<n;i++)
-	{
-		printf("enter %d pocess id\n",i+1);
-		scanf("%d",&p[i].processid);
-		printf("enter arrival time\n");
-		scanf("%d",&p[i].arrivaltime);
-	}
-	
-	for(int i=0;i<n;i++)
-	{
-		p[i].bursttime=p[i].arrivaltime*2;
-	}
-	
-	int k=1;
-	for(int i=0;i<n;i++)
-	{
-	while(p[i].bursttime!=0)
-	{
-		k++;
-		p[i].bursttime--;
-		printf("%d",p[i].processid);
-	}
-	p[i].ct=k;
-    }
-    printf("Output is \n");
-    
-    for(int i=0;i<n;i++)
-    {
-    printf("%d",p[i].ct);
-    }
+
+int i,n,p[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
+
+int bt[10],temp,j,arr[10],wait[10],turn[10],ta=0,sum=0;
+
+float wavg=0,tavg=0,tsum=0,wsum=0;
+
+printf("\t\t\tShortest Job First Scheduling (Non - Preemptive)\n");
+
+printf("\nEnter the No. of processes :");
+
+scanf("%d",&n);
+
+printf("Enter the arrival time and burst time of the processes:\n");
+
+for(i=0;i<n;i++)
+
+scanf(" %d %d",&arr[i],&bt[i]);
+
+for(i=0;i<n;i++)
+
+{
+
+for(j=0;j<n;j++)
+
+{
+
+if(arr[i]<arr[j])
+
+{
+
+temp=p[j];
+
+p[j]=p[i];
+
+p[i]=temp;
+
+temp=arr[j];
+
+arr[j]=arr[i];
+
+arr[i]=temp;
+
+temp=bt[j];
+
+bt[j]=bt[i];
+
+bt[i]=temp;
+
 }
 
+}
+
+}
+
+for(j=0;j<n;j++)
+
+{
+
+btime=btime+bt[j];
+
+min=bt[k];
+
+for(i=k;i<n;i++)
+
+{
+
+if (btime>=arr[i] && bt[i]<min)
+
+{
+
+temp=p[k];
+
+p[k]=p[i];
+
+p[i]=temp;
+
+temp=arr[k];
+
+arr[k]=arr[i];
+
+arr[i]=temp;
+
+temp=bt[k];
+
+bt[k]=bt[i];
+
+bt[i]=temp;
+
+}
+
+}
+
+k++;
+
+}
+
+wait[0]=0;
+
+for(i=1;i<n;i++)
+
+{
+
+sum=sum+bt[i-1];
+
+wait[i]=sum-arr[i];
+
+wsum=wsum+wait[i];
+
+}
+
+wavg=(wsum/n);
+
+for(i=0;i<n;i++)
+
+{
+
+ta=ta+bt[i];
+
+turn[i]=ta-arr[i];
+
+tsum=tsum+turn[i];
+
+}
+
+tavg=(tsum/n);
+
+printf("************************");
+
+printf("\n RESULT:-");
+
+printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
+
+for(i=0;i<n;i++)
+
+printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",p[i],bt[i],arr[i],wait[i],turn[i]);
+
+printf("\n\nAVERAGE WAITING TIME : %f",wavg);
+
+printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
+
+return 0;
+
+}
