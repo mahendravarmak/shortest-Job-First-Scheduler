7c7acf1e897be64fd608d0e580cd0dbe4a201f77
mahendravarmak <49528392+mahendravarmak@users.noreply.github.com>
2019-04-16 23:58:10 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,149 +1,123 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+struct proc
+{
+	char processid[10][10];
+	int bursttime;
+	int arrivaltime=0;
+	int tat;
+	int wt;
+	int ct=0;
+};
 
 int main()
-
 {
+	int n;
+	float avgtat=0,avgwt=0;
+	printf("enter how many process u want to enter\n");
+	scanf("%d",&n);
+	proc p[n];
+	for(int i=0;i<n;i++)
+	{
+		printf("enter %d pocess id\n",i+1);
+		scanf("%s",&p[i].processid[i]);
+		printf("enter arrival time\n");
+		scanf("%d",&p[i].arrivaltime);
+	}
+	
+	
+	
+	for(int i=0;i<n;i++)
+	{
+		p[i].bursttime=p[i].arrivaltime*2;
+	}
+	
+	int k=1;
+	for(int i=0;i<n;i++)
+	{	
+	int temp=p[i].bursttime;
+	while(temp!=0)
+	{
+		k++;
+		temp--;
+	//	printf("%s\t",p[i].processid[i]);
+	}
+	
+	p[i].ct=k;
+    
+	}
+    printf("\n");
+    
+    for(int i=0;i<n;i++)
+	{
+		p[i].tat=p[i].ct-p[i].arrivaltime;
+	}
 
-int i,n,p[10]={1,2,3,4,5,6,7,8,9,10},min,k=1,btime=0;
+    for(int i=0;i<n;i++)
+     {
+	 p[i].wt=p[i].tat-p[i].bursttime;
+    }
+    
+    system("cls");
+    printf("\n\n");
+    
+    printf("\t\t\t");
+    for(int i=0;i<n;i++)
+    {
+    	printf("%s\t",p[i].processid[i]);
+	}  
+	printf("\n");
+	printf("\n");
+	
+   printf("arrival time is:\t");
+    for(int i=0;i<n;i++)
+    {
+    	printf("%d\t",p[i].arrivaltime);
+	} 
+    printf("\n");
+    
+    printf("burst time is:\t\t");
+    for(int i=0;i<n;i++)
+    {
+    printf("%d\t",p[i].bursttime);
+    }
+    printf("\n");
 
-int bt[10],temp,j,arr[10],wait[10],turn[10],ta=0,sum=0;
 
-float wavg=0,tavg=0,tsum=0,wsum=0;
-
-printf("\t\t\tShortest Job First Scheduling (Non - Preemptive)\n");
-
-printf("\nEnter the No. of processes :");
-
-scanf("%d",&n);
-
-printf("Enter the arrival time and burst time of the processes:\n");
-
-for(i=0;i<n;i++)
-
-scanf(" %d %d",&arr[i],&bt[i]);
-
-for(i=0;i<n;i++)
-
-{
-
-for(j=0;j<n;j++)
-
-{
-
-if(arr[i]<arr[j])
-
-{
-
-temp=p[j];
-
-p[j]=p[i];
-
-p[i]=temp;
-
-temp=arr[j];
-
-arr[j]=arr[i];
-
-arr[i]=temp;
-
-temp=bt[j];
-
-bt[j]=bt[i];
-
-bt[i]=temp;
-
+	printf("completion time is:\t");
+    for(int i=0;i<n;i++)
+    {
+    printf("%d\t",p[i].ct);
+    }
+    printf("\n");
+   
+   
+   printf("turn around time is:\t");
+    for(int i=0;i<n;i++)
+    {
+    printf("%d\t",p[i].tat);
+    }
+    printf("\n");
+    
+    printf("waiting time\t\t");
+    for(int i=0;i<n;i++)
+    {
+    printf("%d\t",p[i].wt);
+    }
+    printf("\n");
+   
+    for(int i=0;i<n;i++)
+	{
+		avgtat+=p[i].tat;
+		avgwt+=p[i].wt;
+	}
+	
+    avgtat=avgtat/n;
+    avgwt=avgwt/n;
+	printf("\n");
+	printf("average turnaroundtime time is:%.2f \n",avgtat);
+	printf("average waiting time is: %.2f \n",avgwt);
 }
 
-}
-
-}
-
-for(j=0;j<n;j++)
-
-{
-
-btime=btime+bt[j];
-
-min=bt[k];
-
-for(i=k;i<n;i++)
-
-{
-
-if (btime>=arr[i] && bt[i]<min)
-
-{
-
-temp=p[k];
-
-p[k]=p[i];
-
-p[i]=temp;
-
-temp=arr[k];
-
-arr[k]=arr[i];
-
-arr[i]=temp;
-
-temp=bt[k];
-
-bt[k]=bt[i];
-
-bt[i]=temp;
-
-}
-
-}
-
-k++;
-
-}
-
-wait[0]=0;
-
-for(i=1;i<n;i++)
-
-{
-
-sum=sum+bt[i-1];
-
-wait[i]=sum-arr[i];
-
-wsum=wsum+wait[i];
-
-}
-
-wavg=(wsum/n);
-
-for(i=0;i<n;i++)
-
-{
-
-ta=ta+bt[i];
-
-turn[i]=ta-arr[i];
-
-tsum=tsum+turn[i];
-
-}
-
-tavg=(tsum/n);
-
-printf("************************");
-
-printf("\n RESULT:-");
-
-printf("\nProcess\t Burst\t Arrival\t Waiting\t Turn-around" );
-
-for(i=0;i<n;i++)
-
-printf("\n p%d\t %d\t %d\t\t %d\t\t\t%d",p[i],bt[i],arr[i],wait[i],turn[i]);
-
-printf("\n\nAVERAGE WAITING TIME : %f",wavg);
-
-printf("\nAVERAGE TURN AROUND TIME : %f",tavg);
-
-return 0;
-
-}
